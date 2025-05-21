9a833566fcef8963b9b638770174fb0782a595f0
mrczarny <adamski.jj@gmail.com>
2025-05-21 14:43:06 +0200

    voice

--- services/voiceRecon_service.py
+++ services/voiceRecon_service.py
@@ -1,7 +1,29 @@
 from celery import shared_task
+import asyncio
+import os
+import speech_recognition as sr
 
 
-class VoiceReconService():
-    @shared_task(ignore_result=False)
-    def add_together(a: int, b: int) -> int:
-        return a + b
+
+class VoiceReconService:
+    def __init__(self):
+        self.recognizer = sr.Recognizer()
+        self.microphone = sr.Microphone()
+        # self.loop = asyncio.new_event_loop()
+        # asyncio.set_event_loop(self.loop)
+    def recognize_voice(self) -> str:
+        
+        with self.microphone as source:
+            print("Say something!")
+            audio =  self.recognizer.listen(source)
+        # recognize speech using Google Speech Recognition
+        try:
+            d = self.recognizer.recognize_google(audio)
+            return d
+        except Exception as e:
+            print("Could not understand audio")
+            return None
+    
+    def recognize_voice_task(self):
+        result = self.loop.run_until_complete(self.recognize_voice())
+        return result
